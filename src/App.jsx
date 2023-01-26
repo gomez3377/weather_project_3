31977965556c70f169c91df7a215c876ddb19c24
gomez3377 <jongomez3377@gmail.com>
2023-01-26 00:46:39 -0700

    added color set default font

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   
 
   return (
-    <div className="App">
+    <div className="App font-sans">
       <Sidebar/>
       <Main/>
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      midnightBlue: "#100E1D",
+      darkBlue: "#1E213A",
+      goldYellow: "#FFEC65",
+      gray: "#6E707A",
+      nearWhite: "#E7E7EB",
+      lightGray: "#A09FB1",
+      lightBlue: "#585676",
+      blue: "#3C47E9",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
