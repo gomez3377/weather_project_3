2c0f4ce83a99343c15bfa161b608521d4748c98d
gomez3377 <jongomez3377@gmail.com>
2023-01-26 00:30:33 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+import Main from './components/Main'
+import Sidebar from './components/Sidebar'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Sidebar/>
+      <Main/>
+
+    </div>
+  )
+}
+
+export default App
+
+// colors
+// midnight-blue #100E1D
+// dark-blue #1E213A
+// gold-yellow #FFEC65
+// gray #6E707A
+// near-white #E7E7EB
+// light-gray #A09FB1
+// light-blue ##585676
+// blue #3C47E9
